22cc67e98c38267c9411d8a71d563242a36564ba
jiggum <0s.dongmin@gmail.com>
2016-06-30 23:07:51 +0900

    delete adress function

--- app/src/main/java/com/example/q/myapplication/Tab_C.java
+++ app/src/main/java/com/example/q/myapplication/Tab_C.java
@@ -207,26 +207,6 @@ public class Tab_C extends Fragment implements OnMapReadyCallback{
         }
     }
 
-    public String getAddress(Context context, double lat, double lng) {
-        Geocoder geocoder = new Geocoder(context, Locale.getDefault());
-        try {
-            List<Address> addresses = geocoder.getFromLocation(lat, lng, 1);
-            Address obj = addresses.get(0);
-
-            String add = obj.getAddressLine(0);
-            add = add + "\n" + obj.getCountryName();
-            add = add + "\n" + obj.getCountryCode();
-            add = add + "\n" + obj.getAdminArea();
-            add = add + "\n" + obj.getPostalCode();
-            add = add + "\n" + obj.getSubAdminArea();
-            add = add + "\n" + obj.getLocality();
-            add = add + "\n" + obj.getSubThoroughfare();
-
-            return add;
-        } catch (IOException e) {
-            return null;
-        }
-    }
 
 
 }
